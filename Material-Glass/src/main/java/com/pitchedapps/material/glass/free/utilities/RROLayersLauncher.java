5285b2adb82e66d7e999da0a565df1fb3bd518d8
asdfasdfvful <asdfasdfvful@gmail.com>
2015-07-02 23:14:31 -0700

    failed fab for layers

--- Material-Glass/src/main/java/com/pitchedapps/material/glass/free/utilities/RROLayersLauncher.java
+++ Material-Glass/src/main/java/com/pitchedapps/material/glass/free/utilities/RROLayersLauncher.java
@@ -8,7 +8,11 @@ public class RROLayersLauncher {
     public RROLayersLauncher(Context context) {
         Intent intent = new Intent("android.intent.action.MAIN");
         intent.setComponent(new ComponentName("com.lovejoy777.rroandlayersmanager", "com.lovejoy777.rroandlayersmanager.menu"));
-        intent.putExtra("pkgName", "com.pitchedapps.material.glass.free.Overlay");
+        intent.putExtra("pkgName", "com.pitchedapps.material.glass.free/.Overlay");
+        //original:         intent.putExtra("pkgName", context.getPackageName());
+        // tried package name with ".Overlay" or "/.Overlay". All don't work
+        //Failure retrieving resources for com.pitchedapps.material.glass.free: Resource ID #0x0
+        //Not sure if this is related to my theme or the way the layers manager gets the drawable
         context.startActivity(intent);
     }
 }
